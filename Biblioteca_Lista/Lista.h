bf47ee03e73c2bb44adab2611124041347633b9b
habner-gabryel <habner.gabryel.cc@gmail.com>
2026-06-02 20:49:33 -0300

    feat: adicionando includes no arquivo Lista.h

--- Biblioteca_Lista/Lista.h
+++ Biblioteca_Lista/Lista.h
@@ -9,15 +9,21 @@
 
 #include "Operacoes/2_incluirInfo.h"
 
-#include  "Operacoes/4_contemInfo.h"
+#include "Operacoes/3_excluirInfo.h"
 
-#include  "Operacoes/5_imprimirLista.h"
+#include "Operacoes/4_contemInfo.h"
 
-#include  "Operacoes/7_duplicarLista.h"
+#include "Operacoes/5_imprimirLista.h"
 
-#include  "Operacoes/6_destruirLista.h"
-#include  "Operacoes/6_1_destruirListaInfo.h"
+#include "Operacoes/7_duplicarLista.h"
 
-#include  "Operacoes/9_unirListas.h"
+#include "Operacoes/6_destruirLista.h"
+#include "Operacoes/6_1_destruirListaInfo.h"
+
+#include "Operacoes/7_duplicarLista.h"
+
+#include "Operacoes/8_dividirLista.h"
+
+#include "Operacoes/9_unirListas.h"
 
 #endif
