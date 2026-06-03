008b2a0068a8e5d280f9f7d04bc5d255011f3f00
habner-gabryel <habner.gabryel.cc@gmail.com>
2026-06-02 21:07:23 -0300

    feat: implementação da função incluirInfoOrdenada

--- Biblioteca_Lista/Operacoes/2_incluirInfo.h
+++ Biblioteca_Lista/Operacoes/2_incluirInfo.h
@@ -3,63 +3,89 @@
 
 void incluirInfoInicio(pDLista pd, void* info){
 
-       pNoh novoNoh;
-       novoNoh = malloc(sizeof(Noh));
+   pNoh novoNoh;
+   novoNoh = malloc(sizeof(Noh));
 
-       novoNoh->info = info;
-       novoNoh->prox = pd->inicio;
+   novoNoh->info = info;
+   novoNoh->prox = pd->inicio;
 
-       if(pd->inicio == NULL)
-          pd->fim = novoNoh;
+   if(pd->inicio == NULL)
+      pd->fim = novoNoh;
 
-       pd->inicio = novoNoh;
+   pd->inicio = novoNoh;
 }
 
 void incluirInfoMeio(pDLista pd, void* info, int pos){
 
-       pNoh novoNoh;
-       novoNoh = malloc(sizeof(Noh));
+   pNoh novoNoh;
+   novoNoh = malloc(sizeof(Noh));
 
-       novoNoh->info = info;
+   novoNoh->info = info;
 
-       if(pos == 0){
-          novoNoh->prox = pd->inicio;
-          pd->inicio = novoNoh;
-          if(pd->fim == NULL)
-             pd->fim = novoNoh;
-       }
-       else{
-          pNoh aux = pd->inicio;
-          int i;
+   if(pos == 0){
+      novoNoh->prox = pd->inicio;
+      pd->inicio = novoNoh;
+      if(pd->fim == NULL)
+         pd->fim = novoNoh;
+   } else {
+      pNoh aux = pd->inicio;
+      int i;
 
-          for(i=0; i<pos-1 && aux != NULL; i++)
-              aux = aux->prox;
+      for(i=0; i<pos-1 && aux != NULL; i++)
+         aux = aux->prox;
 
-          if(aux != NULL){
-             novoNoh->prox = aux->prox;
-             aux->prox = novoNoh;
-             if(novoNoh->prox == NULL)
-                pd->fim = novoNoh;
-          }
-          else
-             free(novoNoh);
-       }
+      if(aux != NULL){
+         novoNoh->prox = aux->prox;
+         aux->prox = novoNoh;
+         if(novoNoh->prox == NULL)
+            pd->fim = novoNoh;
+      } else {
+         free(novoNoh);
+      }
+   }
 } 
 
 void incluirInfoFim(pDLista pd, void* info){
 
-       pNoh novoNoh;
-       novoNoh = malloc(sizeof(Noh));
+   pNoh novoNoh;
+   novoNoh = malloc(sizeof(Noh));
 
-       novoNoh->info = info;
-       novoNoh->prox = NULL;
+   novoNoh->info = info;
+   novoNoh->prox = NULL;
 
-       if(pd->inicio == NULL)
-          pd->inicio = novoNoh;
-       else
-          pd->fim->prox = novoNoh;
+   if(pd->inicio == NULL)
+      pd->inicio = novoNoh;
+   else
+      pd->fim->prox = novoNoh;
 
-       pd->fim = novoNoh;
+   pd->fim = novoNoh;
+}
+
+void incluirInfoOrdenada(pDLista pd, void* info, FuncaoComparacao funcaoComparacao){
+
+   pNoh novoNoh;
+   novoNoh = malloc(sizeof(Noh));
+
+   novoNoh->info = info;
+
+   if(pd->inicio == NULL || funcaoComparacao(info, pd->inicio->info) < 0){
+      novoNoh->prox = pd->inicio;
+      pd->inicio = novoNoh;
+      if(pd->fim == NULL)
+         pd->fim = novoNoh;
+   } else {
+      pNoh aux = pd->inicio;
+
+      while(aux->prox != NULL && funcaoComparacao(info, aux->prox->info) >= 0){
+         aux = aux->prox;
+      }
+
+      novoNoh->prox = aux->prox;
+      aux->prox = novoNoh;
+      if(novoNoh->prox == NULL) {
+         pd->fim = novoNoh;
+      }
+   }
 }
 
 #endif
