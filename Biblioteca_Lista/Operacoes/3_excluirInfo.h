ee950b878f26b03686c4dd8c7dcd6c195fccef61
habner-gabryel <habner.gabryel.cc@gmail.com>
2026-06-02 21:33:32 -0300

    feat: implementando excluirInfo

--- Biblioteca_Lista/Operacoes/3_excluirInfo.h
+++ Biblioteca_Lista/Operacoes/3_excluirInfo.h
@@ -3,6 +3,31 @@
 
 int excluirInfo(pDLista pd, void* info){
 
+    pNoh atual = pd->inicio;
+
+    while(atual != NULL){
+        if(atual->info == info){
+            if(atual == pd->inicio){
+                pd->inicio = atual->prox;
+                if(pd->fim == atual){
+                    pd->fim = NULL;
+                }
+            } else {
+                pNoh aux = pd->inicio;
+                while(aux->prox != atual){
+                    aux = aux->prox;
+                }
+                aux->prox = atual->prox;
+                if(pd->fim == atual){
+                    pd->fim = aux;
+                }
+            }
+            free(atual);
+            pd->quantidade--;
+            return 1;
+        }
+        atual = atual->prox;
+    }
 
     return 0;
 }
