a489d5b10d2ee0a9e3e89bdba99707e82646bdee
habner-gabryel <habner.gabryel.cc@gmail.com>
2026-06-02 21:42:07 -0300

    feat: implementando destruirListaInfo e removendo códigos duplicados

--- Biblioteca_Lista/Operacoes/6_destruirLista.h
+++ Biblioteca_Lista/Operacoes/6_destruirLista.h
@@ -1,37 +1,33 @@
 #ifndef DESTRUIR_LISTA_H
 #define DESTRUIR_LISTA_H
 
-void destruirListaRecursiva(pNoh atual){
-
-    if (atual == NULL)
+void destruirListaRecursiva(pNoh atual, FuncaoLiberacao funcaoLiberacao) {
+    if (atual == NULL){
         return;
+    }
 
-    destruirListaRecursiva(atual->prox);
+    destruirListaRecursiva(atual->prox, funcaoLiberacao);
+    if(funcaoLiberacao != NULL) {
+        funcaoLiberacao(atual->info);
+    }
     free(atual);
 }
 
 /* -------------------------------------*/
 void destruirLista(pDLista pd){
+    destruirListaRecursiva(pd->inicio, NULL);
+    atualizarDescritor(pd);
+}
 
-/*
-    pNoh atual = pd->inicio;
-    while(atual != NULL){
+void destruirListaInfo(pDLista pd, FuncaoLiberacao funcaoLiberacao){
+    destruirListaRecursiva(pd->inicio, funcaoLiberacao);
+    atualizarDescritor(pd);
+}
 
-        pNoh aux = atual->prox;
-
-        free(atual);
-
-        atual = aux;
-    }
-*/
-   destruirListaRecursiva(pd->inicio);
-
-
-    // atualizar o descritor
+void atualizarDescritor(pDLista pd){
     pd->quantidade = 0;
     pd->inicio     = NULL;
     pd->fim        = NULL;
-
 }
 
 #endif
