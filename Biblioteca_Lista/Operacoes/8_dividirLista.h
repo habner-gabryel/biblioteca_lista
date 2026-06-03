39c19e7a85c340ee7b0d3965960c350b2b4e451f
habner-gabryel <habner.gabryel.cc@gmail.com>
2026-06-02 21:02:28 -0300

    feat: implementando função dividirLista

--- Biblioteca_Lista/Operacoes/8_dividirLista.h
+++ Biblioteca_Lista/Operacoes/8_dividirLista.h
@@ -5,7 +5,29 @@
 pDLista dividirLista(pDLista pListaOriginal, int pos)
 {
 
-    return NULL;
+    pDLista pListaDividida = criarDLista();
+
+    pNoh atual = pListaOriginal->inicio;
+    pNoh aux = NULL;
+    int cont = 0;
+
+    while(atual != NULL && cont < pos){
+        atual = atual->prox;
+        cont++;
+    }
+
+    if(atual != NULL){
+        aux = atual;
+        aux->prox = NULL;
+        pListaOriginal->fim         = aux;
+        pListaOriginal->quantidade  = pos;
+
+        pListaDividida->inicio      = atual->prox;
+        pListaDividida->quantidade  = pListaOriginal->quantidade - pos;
+        pListaDividida->fim         = pListaOriginal->fim;
+    }
+
+    return pListaDividida;
 }
 
 #endif
